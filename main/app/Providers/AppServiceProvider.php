fddec8ae724151877b31973a11415d5af13a174e
akandeBolaji <akandbolajidanielofficial@gmail.com>
2019-12-19 02:39:22 +0100

    chore(env setup)

--- main/app/Providers/AppServiceProvider.php
+++ main/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Schema\Builder; // Import Builder where defaultStringLength method is defined
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Builder::defaultStringLength(191); // Update defaultStringLength
     }
 }
