407f735f6230b4c9f0e01bcf097071efd76067fb
leinad <xavi7th@gmail.com>
2020-02-12 19:39:56 +0100

    remove the must verify trait from User model

--- main/app/User.php
+++ main/app/User.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable //implements MustVerifyEmail
 {
 	use Notifiable, SoftDeletes, Rememberable;
 
