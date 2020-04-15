6be5bdfc7e9ef19a9c8b17800539359007483fa1
leinad <xavi7th@gmail.com>
2020-04-15 16:00:45 +0100

    Set up notifications

---
+++ main/app/Modules/AppUser/Notifications/WithdrawalRequestCreatedNotification.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Modules\AppUser\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class WithdrawalRequestCreatedNotification extends Notification
+{
+	use Queueable;
+
+	/**
+	 * Create a new notification instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		//
+	}
+
+	/**
+	 * Get the notification's delivery channels.
+	 *
+	 * @param mixed $notifiable
+	 * @return array
+	 */
+	public function via($notifiable)
+	{
+		return ['database'];
+	}
+
+	/**
+	 * Get the mail representation of the notification.
+	 *
+	 * @param mixed $notifiable
+	 * @return \Illuminate\Notifications\Messages\MailMessage
+	 */
+	public function toMail($user)
+	{
+		return (new MailMessage)
+			->line('The introduction to the notification.')
+			->action('Notification Action', 'https://laravel.com')
+			->line('Thank you for using our application!');
+	}
+
+	/**
+	 * Get the database representation of the notification.
+	 */
+	public function toDatabase($user)
+	{
+		return [
+			'action' => 'Your withdrawal request has been created. We will update you once it has been attended to.'
+		];
+	}
+}
