9189dc403e3e115957cff9b2e1c57f603ed6b662
leinad <xavi7th@gmail.com>
2020-05-17 13:44:23 +0100

    Fix: Make menu item have skippable elements

--- main/app/Modules/BasicSite/Http/Controllers/BasicSiteController.php
+++ main/app/Modules/BasicSite/Http/Controllers/BasicSiteController.php
@@ -31,73 +31,24 @@ class BasicSiteController extends Controller
   public static function routes()
   {
     Route::group(['middleware' => 'web', 'namespace' => 'App\\Modules\BasicSite\Http\Controllers'], function () {
-
-      Route::get('/site/setup/{key?}',  function ($key = null) {
-
-        if ($key == config('app.migration_key')) {
-          // dd(config('app.migration_key'));
-
-          try {
-            echo '<br>init storage:link...';
-            $rsp = Artisan::call('storage:link');
-            echo 'done storage:link. Result: ' . $rsp;
-
-            echo '<br>init migrate:fresh...';
-            $rsp =  Artisan::call('migrate:fresh');
-            echo 'done migrate:fresh. Result: ' . $rsp;
-
-            echo '<br>init module:seed...';
-            $rsp =  Artisan::call('module:seed');
-            echo 'done module:seed. Result: ' . $rsp;
-          } catch (Exception $e) {
-            Response::make($e->getMessage(), 500);
-          }
-        } else {
-          App::abort(404);
-        }
-      });
-
-      Route::get('/', [BasicSiteController::class, 'index'])->name('app.home');
+      Route::get('/', [BasicSiteController::class, 'index'])->name('app.home')->defaults('nav_skip', true);
       Route::get('/blog', [BasicSiteController::class, 'blog'])->name('app.blog');
       Route::get('/frequently-asked-questions', [BasicSiteController::class, 'faqs'])->name('app.faqs');
       Route::get('/careers', [BasicSiteController::class, 'careers'])->name('app.careers');
-      Route::get('/privacy', [BasicSiteController::class, 'showContactForm'])->name('app.privacy');
-      Route::get('/terms-and-conditions', [BasicSiteController::class, 'showContactForm'])->name('app.terms');
+      Route::get('/privacy', [BasicSiteController::class, 'showContactForm'])->name('app.privacy')->defaults('nav_skip', true);
+      Route::get('/terms-and-conditions', [BasicSiteController::class, 'showContactForm'])->name('app.terms')->defaults('nav_skip', true);
       Route::get('/contact-us', [BasicSiteController::class, 'showContactForm'])->name('app.contact_us');
       Route::post('/contact', [BasicSiteController::class, 'sendContactMessage'])->name('app.contact');
-    });
-
-
-    Route::group(['middleware' => 'web', 'prefix' => 'api'], function () {
-
-      Route::get('testimonials', function () {
-        $testimonials = Testimonial::all();
-
-        return (new TestimonialTransformer)->collectionTransformer($testimonials, 'transformForHomePage');
-      });
-
-      Route::get('team', function () {
-        $teams = TeamMember::all();
-
-        return (new TeamMemberTransformer)->collectionTransformer($teams, 'transformForHomePage');
-      });
-
-      Route::get('faq', function () {
-        // $teams = TeamMember::all();
-
-        // return (new TeamMemberTransformer)->collectionTransformer($teams, 'transformForHomePage');
-      });
-
-      Route::post('/contact', function (ContactFormValidation $request) {
-        Message::create($request->all());
-        return response()->json(['status' => true], 201);
-      });
     });
   }
 
   public function index()
   {
-    return Inertia::render('HomePage');
+    // dd(\Illuminate\Support\Facades\Route::getRoutes()->get()[100]->defaults);
+    return Inertia::render('HomePage', [
+      // 'testimonials' => (new TestimonialTransformer)->collectionTransformer(Testimonial::all(), 'transformForHomePage'),
+      // 'team_members' => (new TeamMemberTransformer)->collectionTransformer(TeamMember::all(), 'transformForHomePage')
+    ]);
   }
 
   public function blog()
@@ -120,8 +71,9 @@ class BasicSiteController extends Controller
     return Inertia::render('ContactUsPage');
   }
 
-  public function sendContactMessage()
+  public function sendContactMessage(ContactFormValidation $request)
   {
-    return Inertia::render('HomePage');
+    Message::create($request->all());
+    return response()->json(['status' => true], 201);
   }
 }
