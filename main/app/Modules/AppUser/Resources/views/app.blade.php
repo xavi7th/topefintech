14d498a62363831df9d5b4bb2949795eed5962cd
leinad <xavi7th@gmail.com>
2020-06-25 07:10:32 +0100

    Added tawk script

--- main/app/Modules/AppUser/Resources/views/app.blade.php
+++ main/app/Modules/AppUser/Resources/views/app.blade.php
@@ -263,6 +263,23 @@
     </div>
   </div>
   @inertia
+
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+    var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+    (function() {
+      var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = 'https://embed.tawk.to/5eef6fdd4a7c6258179b0d34/default';
+      s1.charset = 'UTF-8';
+      s1.setAttribute('crossorigin', '*');
+      s0.parentNode.insertBefore(s1, s0);
+    })();
+  </script>
+  <!--End of Tawk.to Script-->
+
 </body>
 
 </html>
