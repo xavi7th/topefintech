7a51fb0e652351c9f50a0489dc34033629507416
leinad <xavi7th@gmail.com>
2020-06-28 11:59:57 +0100

    Fix: Can't "go back" to public pages after going to login or register pages

---
+++ main/app/Modules/BasicSite/Resources/js/components/auth/Register.vue
@@ -0,0 +1,14 @@
+<template>
+  <layout :isAuth="true" title="Login"></layout>
+</template>
+
+<script>
+  import Layout from "@dashboard-assets/js/AppComponent";
+  export default {
+    name: "RegisterPage",
+    beforeCreate() {
+      location.reload();
+    },
+    components: { Layout }
+  };
+</script>
